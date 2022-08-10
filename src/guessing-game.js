87313f1009554cb93e4a73b08ce9ab464ad146ea
mafiti91 <ppivankov@gmail.com>
2022-08-10 10:26:12 +0300

    colution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        return this.result = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        return this.max = this.result;
     }
 
     greater() {
-
+        return this.min = this.result;
     }
 }
 
